370146d8e61cc54682ef9e9e914adb4e2f857d39
aitanpay1996 <realpanggeai@gmail.com>
2026-06-03 14:54:19 +0700

    Update send.py

--- send.py
+++ send.py
@@ -4,7 +4,16 @@ import os
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
 
-MESSAGE = "Halo grup, ini pesan otomatis dari GitHub Actions."
+MESSAGE = """High-Quality Guestpost Available 🔥
+
+Metrik kuat, harga tetap ramah di kantong 💸
+Konten full English, siap publish profesional 🌍
+
+Cukup Rp 250.000 per post aja
+
+Cocok banget buat boost authority & ranking website kamu 🚀
+
+Langsung order atau tanya detail ke @karyaonedigital"""
 
 url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
